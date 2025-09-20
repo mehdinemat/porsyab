ac3da00769aaddcb1962747baff30519f64d4d91
nematnejad <nematnejad>
2025-09-20 12:38:15 +0330

    add

--- components/base/global.js
+++ components/base/global.js
@@ -19,6 +19,15 @@ export const Fonts = ({ lang }) => (
         font-display: swap;
       }
 
+       
+      @font-face {
+        font-family: 'morabba';
+        src: url('/fonts/morabba/Morabba-Regular.ttf') format('truetype');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }
+
       html {
         font-feature-settings: ${lang === "fa" ? `"locl", "ss01"` : "normal"};
       }
